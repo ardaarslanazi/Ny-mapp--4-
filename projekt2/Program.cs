0615c17d5a7f03b4bc806a39f9ae4c1b64a370b7
arda <ardaburc.arslanazi@elev.ga.ntig.se>
2020-09-24 14:02:44 +0200

    first commit

---
+++ projekt2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace projekt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            string fyra;
+            int points = 0;
+        
+
+            Console.WriteLine("Hello! im gonna ask you a couple of questions.");
+            Console.WriteLine("What is 2+2?");
+
+            Console.WriteLine("A = 5");
+            Console.WriteLine("B = 4");
+            Console.WriteLine("C = 6");
+            fyra = Console.ReadLine();
+            if (fyra == "B")
+            {
+                Console.WriteLine("lmao");
+                Console.Writeline("")
+            }
+            else
+            {
+                Console.WriteLine("BEeEeeEEeeP FEL");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Another question.");
+            Console.WriteLine("How many ")
+
+
+
+
+
+
+        }
+    }
+}
